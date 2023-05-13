a751ae6bb7e942b48a1ba10f3a5650885e5b192c
Zachary Freiter <31320978+zfreiter@users.noreply.github.com>
2023-05-13 11:47:47 -0700

    NA

--- server/index.js
+++ server/index.js
@@ -1,11 +1,29 @@
-const express = require('express');
-var cors = require('cors');
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import * as dotenv from 'dotenv';
+import authRoutes from './routes/auth';
+
+dotenv.config();
 const app = express();
-const port = 3000;
-
 app.use(cors());
-app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(port, () =>
-  console.log(`Express app running on port ${port}!`)
-);
+/* ROUTES USING express.Router */
+app.use('/auth', authRoutes);
+
+/* MONGOOSE SETUP */
+const PORT = process.env.PORT || 6001;
+mongoose.set('strictQuery', false);
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () =>
+      console.log(`Server Port: http://localhost:${PORT}`)
+    );
+  })
+  .catch((error) =>
+    console.log(`${error}. Did not connect database`)
+  );
