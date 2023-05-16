180ebb2236a2d2f086c7d600c747690b96546d7a
Cristian Torres Salazar <tor8@pdx.edu>
2023-05-16 10:39:33 -0700

    Code format change

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -20,23 +20,11 @@ export default function App() {
         <Route path="/" element={loggedIn ? <Home /> : <Login />}></Route>
         <Route path="home" element={loggedIn ? <Home /> : <Login />}></Route>
         <Route path="About" element={loggedIn ? <About /> : <Login />}></Route>
-        <Route
-          path="Stocks"
-          element={loggedIn ? <Stocks /> : <Login />}
-        ></Route>
+        <Route path="Stocks" element={loggedIn ? <Stocks /> : <Login />}></Route>
         <Route path="Stock" element={loggedIn ? <Stock /> : <Login />}></Route>
-        <Route
-          path="Account"
-          element={loggedIn ? <Account /> : <Login />}
-        ></Route>
-        <Route
-          path="Crypto"
-          element={loggedIn ? <Crypto /> : <Login />}
-        ></Route>
-        <Route
-          path="Cryptos"
-          element={loggedIn ? <Cryptos /> : <Login />}
-        ></Route>
+        <Route path="Account" element={loggedIn ? <Account /> : <Login />}></Route>
+        <Route path="Crypto" element={loggedIn ? <Crypto /> : <Login />}></Route>
+        <Route path="Cryptos" element={loggedIn ? <Cryptos /> : <Login />}></Route>
         <Route path="*" element={loggedIn ? <NotFound /> : <Login />}></Route>
       </Routes>
     </BrowserRouter>
