8a2b6f8426027d2c6b2a1fd4d08a1bb989f0b315
Cristian Torres Salazar <tor8@pdx.edu>
2023-05-16 10:30:32 -0700

    Initial Navbar with login control complete

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
 import Login from './scenes/Login';
 import Home from './scenes/Home';
 import About from './scenes/About';
@@ -8,25 +9,36 @@ import Account from './scenes/Account';
 import NotFound from './scenes/NotFound';
 import Crypto from './scenes/Crypto';
 import Cryptos from './scenes/Cryptos';
-import Navbar from './scenes/Navbar'
+import Navbar from './scenes/Navbar';
 
-function App() {
+export default function App() {
+  const [loggedIn, setLogin] = useState(false);
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Login />}></Route>
-        <Route path="home" element={<Home />}></Route>
-        <Route path="About" element={<About />}></Route>
-        <Route path="Stocks" element={<Stocks />}></Route>
-        <Route path="Stock" element={<Stock />}></Route>
-        <Route path="Account" element={<Account />}></Route>
-        <Route path="Crypto" element={<Crypto />}></Route>
-        <Route path="Cryptos" element={<Cryptos />}></Route>
-        <Route path="*" element={<NotFound />}></Route>
+        <Route path="/" element={loggedIn ? <Home /> : <Login />}></Route>
+        <Route path="home" element={loggedIn ? <Home /> : <Login />}></Route>
+        <Route path="About" element={loggedIn ? <About /> : <Login />}></Route>
+        <Route
+          path="Stocks"
+          element={loggedIn ? <Stocks /> : <Login />}
+        ></Route>
+        <Route path="Stock" element={loggedIn ? <Stock /> : <Login />}></Route>
+        <Route
+          path="Account"
+          element={loggedIn ? <Account /> : <Login />}
+        ></Route>
+        <Route
+          path="Crypto"
+          element={loggedIn ? <Crypto /> : <Login />}
+        ></Route>
+        <Route
+          path="Cryptos"
+          element={loggedIn ? <Cryptos /> : <Login />}
+        ></Route>
+        <Route path="*" element={loggedIn ? <NotFound /> : <Login />}></Route>
       </Routes>
     </BrowserRouter>
   );
 }
-
-export default App;
