2e4364cdda976989848bc2571526e6e75044777f
Zachary Freiter <31320978+zfreiter@users.noreply.github.com>
2023-05-15 17:50:53 -0700

    fully finished

--- server/controllers/stockGroups.js
+++ server/controllers/stockGroups.js
@@ -3,14 +3,9 @@ import User from '../models/User.js';
 export const updateGroup = async (req, res) => {
   try {
     const { userId, groups } = req.body;
-    let user = await User.findById(userId);
 
-    const newGroup = user.stockGroups;
-    newGroup.splice(0, groups.length, ...groups);
-    user.set('stockGroups', newGroup);
-    // user.stockGroups.splice(0, groups.length, ...groups);
-    //const upDate = User.findByIdAndUpdate(userId, { stockGroups: groups }, { new: true });
-    const upDate = await user.save();
+    const upDate = await User.findByIdAndUpdate(userId, { stockGroups: groups }, { new: true });
+
     res.status(200).json(upDate);
   } catch (err) {
     res.status(404).json({ message: err.message });
--- server/controllers/stocksOwned.js
+++ server/controllers/stocksOwned.js
@@ -3,14 +3,9 @@ import User from '../models/User.js';
 export const updateOwned = async (req, res) => {
   try {
     const { userId, owned } = req.body;
-    let user = await User.findById(userId);
 
-    const newOwned = user.stocksOwned;
-    newOwned.splice(0, owned.length, ...owned);
-    user.set('stocksOwned', newOwned);
+    const upDate = await User.findByIdAndUpdate(userId, { stocksOwned: owned }, { new: true });
 
-    //const upDate = User.findByIdAndUpdate(userId, { stocksOwned: newOwned }, { new: true });
-    const upDate = await user.save();
     res.status(200).json(upDate);
   } catch (err) {
     res.status(404).json({ message: err.message });
