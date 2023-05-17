149b569aaacbd3b076285a02326119097a5d01ad
CristianTS599 <cristiant.salazar@gmail.com>
2023-05-17 14:07:30 -0700

    Made progress on the login page structure

--- client/src/scenes/Login.jsx
+++ client/src/scenes/Login.jsx
@@ -1,3 +1,21 @@
+import Box from '@mui/material/Box';
+import ImageList from '@mui/material/ImageList';
+import CSImage from '../images/city_skyline.jpg';
+
+
 export default function Login() {
-  return <h2>Login Page - Unimplemented</h2>;
+  return <>
+  <Box sx={{width: '100%', display: 'inline-flex', alignItems:'center'}}>
+    <Box sx={{width: '55%', borderStyle: 'none'}}>
+      <ImageList sx={{width: '100%', height:'99vh', overflow:"hidden", marginTop: 0, marginBottom:0}}>
+        <img src={CSImage}/>
+      </ImageList>
+    </Box>
+    <Box sx={{width: '45%', borderStyle: 'none'}}>
+        <Box className="container" sx={{display: 'flex', backgroundColor: 'gainsboro', margin: 'auto', width: '80%', height: '400px', borderRadius: "10px", alignItems: 'center', justifyContent: 'center'}}>
+          test
+        </Box>
+    </Box>
+  </Box>
+  </>;
 }
