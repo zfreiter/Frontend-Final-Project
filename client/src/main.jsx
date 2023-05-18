2101b54c395959473bbc87d3ce240abd96b143e7
Zachary Freiter <31320978+zfreiter@users.noreply.github.com>
2023-05-17 21:11:09 -0700

    Redux state
    Added Redux-toolkit.
    added reducer functions to add token and remove auth token.
    added two reducers to add stockowned and stockgroups.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import CssBaseline from '@mui/material/CssBaseline';
+import { store } from './redux/store.jsx';
+import { Provider } from 'react-redux';
 import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CssBaseline />
-    <App />
+    <Provider store={store}>
+      <CssBaseline />
+      <App />
+    </Provider>
   </React.StrictMode>
 );
