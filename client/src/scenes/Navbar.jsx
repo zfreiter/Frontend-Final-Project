7d9bbce90646ee2029bb4b23a3d3c636eea14a1c
Cristian Torres Salazar <tor8@pdx.edu>
2023-05-31 20:42:44 -0700

    Progress on single stock page

--- client/src/scenes/Navbar.jsx
+++ client/src/scenes/Navbar.jsx
@@ -17,6 +17,7 @@ export default function Navbar() {
           <Button color="inherit"><Link to="/home" style={{textDecoration: 'none', color: 'inherit'}}>Home</Link></Button>
           <Button color="inherit"><Link to="/about" style={{textDecoration: 'none', color: 'inherit'}}>About</Link></Button>
           <Button color="inherit"><Link to="/stocks" style={{textDecoration: 'none', color: 'inherit'}}>stocks</Link></Button>
+          <Button color="inherit"><Link to="/stock:id" style={{textDecoration: 'none', color: 'inherit'}}>stock</Link></Button>
           <Button color="inherit"><Link to="/cryptos" style={{textDecoration: 'none', color: 'inherit'}}>Cryptos</Link></Button>
           <Button color="inherit"><Link to="/account" style={{textDecoration: 'none', color: 'inherit'}}>Account</Link></Button>
         </Stack>
