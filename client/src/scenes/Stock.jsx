c740359e8700a92f1a0ee4558c41f9c444d9b4f9
Cristian Torres Salazar <tor8@pdx.edu>
2023-06-01 23:39:33 -0700

    Completed the stock overview data. charts next

--- client/src/scenes/Stock.jsx
+++ client/src/scenes/Stock.jsx
@@ -1,5 +1,3 @@
-
-
 import {useState} from 'react';
 import Box from '@mui/material/Box' 
 import TextField from '@mui/material/TextField' 
@@ -7,6 +5,44 @@ import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import { PropTypes } from 'prop-types';
 import Typography from '@mui/material/Typography';
+
+// icons
+import TrendingUpIcon from '@mui/icons-material/TrendingUp';
+import TrendingDownIcon from '@mui/icons-material/TrendingDown';
+import CompareArrowsIcon from '@mui/icons-material/CompareArrows';
+import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
+import PercentIcon from '@mui/icons-material/Percent';
+import CurrencyExchangeIcon from '@mui/icons-material/CurrencyExchange';
+import PriceChangeIcon from '@mui/icons-material/PriceChange';
+import KeyboardReturnIcon from '@mui/icons-material/KeyboardReturn';
+import BalanceIcon from '@mui/icons-material/Balance';
+
+const LabelProperties = {
+  style: {fontWeight: 'bolder', fontSize: 'large'}
+}
+
+const InputAttributes = {
+  readOnly:true,
+  disableUnderline: true
+}
+
+const InputStyles = {
+  style:{
+    textAlign: 'center'
+  }
+}
+
+const IconStyleLight = {
+  marginRight: '5px',
+  color: 'floralwhite',
+  backgroundColor: 'cadetblue',
+  borderRadius: '10px'
+}
+
+const AnalyticBoxStyle = {
+  display: 'inline-flex',
+  marginTop: '20px'
+}
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -108,6 +144,14 @@ export default function Stock() {
   }
   GetData();
 
+  const Monify = (toMonify) => {
+    return new Intl.NumberFormat('en-US', {style: 'currency', currency: 'USD'}).format(toMonify);
+  }
+
+  const ToInitialCase = (srcString) => {
+    return srcString.toLowerCase().split(' ').map((word) => word = `${word[0].toUpperCase()}${word.slice(1, word.length)}`).join(' ');
+  }
+
   console.log(stockData);
 
   const [value, setValue] = useState(0);
@@ -121,13 +165,13 @@ export default function Stock() {
       <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
         <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
           <Tab label="Company Overview" {...a11yProps(0)} />
-          <Tab label="Latest Numbers" {...a11yProps(1)} />
+          <Tab label="Latest Analytical" {...a11yProps(1)} />
           <Tab label="Item Three" {...a11yProps(2)} />
         </Tabs>
       </Box>
       <TabPanel value={value} index={0} sx={{height: 'fit-content'}}>
         <Box sx={{borderStyle: 'solid', height: '100%'}}>
-          <Box sx={{alignItems: 'center'}}>
+          <Box sx={{alignItems: 'center', flexWrap: 'wrap'}}>
             <TextField 
               id="CompanyName"
               sx={{width: '100%', marginBottom: '20px'}}
@@ -141,7 +185,7 @@ export default function Stock() {
             <TextField 
               id="Industry"
               label="Industry"
-              defaultValue={`${stockData.Industry}`}
+              defaultValue={ToInitialCase(stockData.Industry)}
               variant="standard"
               sx={{margin: '5px', width: '33%'}}
               InputProps={{readOnly:true}}
@@ -149,7 +193,7 @@ export default function Stock() {
                <TextField 
               id="Sector"
               label="Sector"
-              defaultValue={`${stockData.Sector}`}
+              defaultValue={ToInitialCase(stockData.Sector)}
               variant="standard"
               sx={{margin: '5px', width: '33%'}}
               InputProps={{readOnly:true}}
@@ -157,7 +201,7 @@ export default function Stock() {
               <TextField 
               id="Type"
               label="Type"
-              defaultValue={`${stockData.AssetType.toUpperCase()}`}
+              defaultValue={ToInitialCase(stockData.AssetType.toUpperCase())}
               variant="standard"
               sx={{margin: '5px', width: '33%'}}
               InputProps={{readOnly:true}}
@@ -178,38 +222,171 @@ export default function Stock() {
             <TextField 
               id="CompanyName"
               sx={{width: '100%', marginBottom: '20px'}}
-              label="Company Name"
+              label=""
               defaultValue={`${stockData.Name} (${stockData.Symbol})`}
               variant="standard"
               inputProps={{readOnly:true, style:{textAlign: 'center', fontSize: 'xx-large'}}}
+              InputProps={InputAttributes}
+              InputLabelProps={LabelProperties}
               />
           </Box>
           <Box sx={{display: 'inline-flex', width: '100%', marginBottom: '20px'}}>
             <TextField 
               id="Industry"
               label="Industry"
-              defaultValue={`${stockData.Industry}`}
+              defaultValue={ToInitialCase(stockData.Industry)}
               variant="standard"
               sx={{margin: '5px', width: '33%'}}
               InputProps={{readOnly:true}}
+              InputLabelProps={LabelProperties}
               />
                <TextField 
               id="Sector"
               label="Sector"
-              defaultValue={`${stockData.Sector}`}
+              defaultValue={ToInitialCase(stockData.Sector)}
               variant="standard"
               sx={{margin: '5px', width: '33%'}}
               InputProps={{readOnly:true}}
+              InputLabelProps={LabelProperties}
               />
               <TextField 
               id="Type"
               label="Type"
-              defaultValue={`${stockData.AssetType.toUpperCase()}`}
+              defaultValue={ToInitialCase(stockData.AssetType)}
               variant="standard"
               sx={{margin: '5px', width: '33%'}}
               InputProps={{readOnly:true}}
+              InputLabelProps={LabelProperties}
               />
           </Box>
+        <Box sx={{display: 'flex', justifyContent: 'center', marginBottom: '20px', marginTop: '20px'}}>
+          <Box sx={{display:'flex', flexDirection: 'column', width: '30%', alignItems: 'flex-end'}}>
+            <Box sx={AnalyticBoxStyle}>
+              <TrendingUpIcon sx={IconStyleLight}/>
+              <TextField 
+              id="fiveTwoWeekHigh"
+              label="52 Week High"
+              defaultValue={Monify(stockData['52WeekHigh'])}
+              variant="standard"
+              sx={{width: '100%'}}
+              inputProps={InputStyles}
+              InputProps={InputAttributes}
+              InputLabelProps={LabelProperties}
+              />
+            </Box>
+            <Box sx={AnalyticBoxStyle}>
+              <AttachMoneyIcon sx={IconStyleLight}/>
+              <TextField 
+              id="LatestDividend"
+              label="Last Per-Share Dividend"
+              defaultValue={Monify(stockData.DividendPerShare)}
+              variant="standard"
+              sx={{width: '100%'}}
+              inputProps={InputStyles}
+              InputProps={InputAttributes}
+              InputLabelProps={LabelProperties}
+              />
+            </Box>
+            <Box sx={AnalyticBoxStyle}>
+               <PercentIcon sx={IconStyleLight}/>
+               <TextField 
+              id="PERatio"
+              label="PE Ratio"
+              defaultValue={stockData.PERatio}
+              variant="standard"
+              sx={{width: '100%'}}
+              inputProps={InputStyles}
+              InputProps={InputAttributes}
+              InputLabelProps={LabelProperties}
+              />
+            </Box>
+          </Box>
+          <Box sx={{display:'flex', flexDirection: 'column', width: '30%', alignItems: 'center'}}>
+            <Box sx={AnalyticBoxStyle}>
+              <TrendingDownIcon sx={IconStyleLight}/>
+              <TextField 
+              id="fivetwoWeekLow"
+              label="50 Week Low"
+              defaultValue={Monify(stockData['52WeekLow'])}
+              variant="standard"
+              sx={{width: '100%'}}
+              inputProps={InputStyles}
+              InputProps={InputAttributes}
+              InputLabelProps={LabelProperties}
+              />
+            </Box>
+            <Box sx={AnalyticBoxStyle}>
+              <CurrencyExchangeIcon sx={IconStyleLight}/>
+              <TextField 
+              id="DividendYield"
+              label="Dividend Yield"
+              defaultValue={stockData.DividendYield}
+              variant="standard"
+              sx={{width: '100%'}}
+              inputProps={InputStyles}
+              InputProps={InputAttributes}
+              InputLabelProps={LabelProperties}
+              />
+            </Box>
+            <Box sx={AnalyticBoxStyle}>
+              <PriceChangeIcon sx={IconStyleLight}/> 
+              <TextField 
+              id="ProfitMargin"
+              label="Profit Margin"
+              defaultValue={stockData.ProfitMargin}
+              variant="standard"
+              sx={{width: '100%'}}
+              inputProps={InputStyles}
+              InputProps={InputAttributes}
+              InputLabelProps={LabelProperties}
+              />
+            </Box>
+          </Box>
+          <Box sx={{display:'flex', flexDirection: 'column', width: '30%', alignItems: 'flex-start'}}>
+            <Box sx={AnalyticBoxStyle}>
+              <CompareArrowsIcon sx={IconStyleLight}/>
+              <TextField 
+              id="MovingAVG"
+              label="50 Day Moving Average"
+              defaultValue={Monify(stockData['50DayMovingAverage'])}
+              variant="standard"
+              sx={{width: '100%'}}
+              inputProps={InputStyles}
+              InputProps={InputAttributes}
+              InputLabelProps={LabelProperties}
+              />
+            </Box>
+            <Box sx={AnalyticBoxStyle}>
+              <KeyboardReturnIcon sx={IconStyleLight}/>
+              <TextField 
+              id="ReturnAsset"
+              label="Return on Assets TTM*"
+              defaultValue={stockData.ReturnOnAssetsTTM}
+              variant="standard"
+              sx={{width: '100%'}}
+              inputProps={InputStyles}
+              InputProps={InputAttributes}
+              InputLabelProps={LabelProperties}
+              />
+            </Box>
+            <Box sx={AnalyticBoxStyle}>
+              <BalanceIcon sx={IconStyleLight}/>
+              <TextField 
+              id="ReturnEquity"
+              label="Return on Equity TTM*"
+              defaultValue={stockData.ReturnOnEquityTTM}
+              variant="standard"
+              sx={{width: '100%'}}
+              inputProps={InputStyles}
+              InputProps={InputAttributes}
+              InputLabelProps={LabelProperties}
+              />
+            </Box>
+          </Box>
+        </Box>
+          <Typography variant='caption' dislay='block' sx={{margin: '10px', fontStyle: 'italic'}}>
+            *TTM - Trailing Twelve Months, PE - Price per Earnings.
+            </Typography>
         </Box>
       </TabPanel>
       <TabPanel value={value} index={2}>
