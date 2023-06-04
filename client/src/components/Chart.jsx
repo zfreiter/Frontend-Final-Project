f080de2c90165cbeb61b71bc469048c0e01d1392
Cristian Torres Salazar <tor8@pdx.edu>
2023-06-03 17:32:20 -0700

    more chart progress

--- client/src/components/Chart.jsx
+++ client/src/components/Chart.jsx
@@ -32,20 +32,28 @@ export const options = {
         text: 'Chart.js Line Chart',
       },
     },
+    maintainAspectRatio: true
   };
 
 
 export default function StockChart({name, symbol, range}) {
+    const [allData, setallData] = useState(null);
+    const [label, setLabels] = useState(null);
+    const [highs, setHighs] = useState(null);
+    const [lows, setLows] = useState(null);
+
     const chartApiURL = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${symbol}&apikey=demo`;
     let apiOn = true;
-    let labels = [];
+    const labels = [];
     let chartDataHigh = [];
     let chartDataLow = [];
-    const GetTimeSeriesData = async () => {
+
+    const GetTimeSeriesData = async (labels, chartDataHigh, chartDataLow) => {
+        try {
         const response = await fetch(chartApiURL);
         const data = await response.json();
         let test1 = await data['Time Series (Daily)'];
-        console.log(test1);
+        console.log('Timer Series data', test1);
 
         for (const [key, value] of Object.entries(test1)) {
             let highVal = value['2. high'];
@@ -53,44 +61,62 @@ export default function StockChart({name, symbol, range}) {
 
             // add label
             labels.push(key);
-
+            
             // add low
             chartDataHigh.push(highVal);
-
+            
             // add high
-            chartDataLow.push(Number(lowVal));
+            chartDataLow.push(lowVal);
         }        
 
         labels = labels.slice(0, range);
+        
         chartDataHigh = chartDataHigh.slice(0, range);
+        
         chartDataLow = chartDataLow.slice(0, range);
-        console.log('');
+
+        setHighs(chartDataHigh);
+        setLabels(labels);
+        setLows(chartDataLow);
+        setallData(test1);
+        }
+        catch (err) {
+            console.log('Error', err);
+        }
     }
 
-    GetTimeSeriesData();
+    useEffect(() => {
+        GetTimeSeriesData(labels, chartDataHigh, chartDataLow);
+    }, []);
+
         
+    console.log('Chart Data high after use efffect', chartDataHigh);
+
+
     let lineData = {
-        labels,
+        label,
         datasets: [
             {
                 label: 'Highs',
-                data: chartDataHigh,
-                borderColor: 'rgb(255, 99, 132)',
+                data: highs,
+                borderColor: 'rgb(0, 0, 0)',
                 backgroundColor: 'rgba(255, 99, 132, 0.5)',
             },
             {
                 label: 'Lows',
-                data: chartDataLow,
+                data: lows,
                 borderColor: 'rgb(53, 162, 235)',
                 backgroundColor: 'rgba(53, 162, 235, 0.5)',
             },
        ],
     };
 
-    console.log(lineData);
+    console.log('chart data before chart render', allData);
+    console.log('labels before chart render', label);
+    console.log('line data chart render', lineData);
     return (
     <>
-        <Line options={options} data={lineData} />
+        {<Line options={options} data={lineData} />}
     </>
     );
 }
